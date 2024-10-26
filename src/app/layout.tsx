10ee3865e220de5ba6b1a309dc409782598ed948
Collin Vesel <cvesel217@gmail.com>
2024-10-26 15:31:20 -0500

    Made the tiles that don't do anything look like they don't do anything.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,12 +37,12 @@ export default function RootLayout({
           <Tile title="Bio" linkTo="/bio" tileStyle={styles.BioTile}/>
           <Tile title="Resume" linkTo="/resume" tileStyle={styles.ResumeTile}/>
           <Tile title="Socials" linkTo="/socials" tileStyle={styles.SocialsTile}/>
-          <Tile title="Featured Updates" tileStyle={styles.FeaturedTile}/>
-          <Tile title="All Projects" tileStyle={styles.ProjectsTile}/>
-          <Tile title="Toybox" tileStyle={styles.ToyboxTile}/>
-          <Tile title="Blog Posts" tileStyle={styles.BlogTile}/>
-          <Tile title="Thoughts" tileStyle={styles.ThoughtsTile}/>
-          <Tile title="Not Mine, but Cool" tileStyle={styles.NotMineTile}/>
+          <Tile title="Coming Soon" tileStyle={`${styles.FeaturedTile} ${styles.DisabledTile}`}/>
+          <Tile title="Coming Soon" tileStyle={`${styles.ProjectsTile} ${styles.DisabledTile}`}/>
+          <Tile title="Coming Soon" tileStyle={`${styles.ToyboxTile} ${styles.DisabledTile}`}/>
+          <Tile title="Coming Soon" tileStyle={`${styles.BlogTile} ${styles.DisabledTile}`}/>
+          <Tile title="Coming Soon" tileStyle={`${styles.ThoughtsTile} ${styles.DisabledTile}`}/>
+          <Tile title="Coming Soon" tileStyle={`${styles.NotMineTile} ${styles.DisabledTile}`}/>
         </main>
         {/* <footer className={styles.CreditNotice}>
           <small>Copyright notice for bg img goes here.</small>
