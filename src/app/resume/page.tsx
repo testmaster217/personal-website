217186ac205873ae2ea7d4c4857d8264a8439c28
Collin Vesel <cvesel217@gmail.com>
2024-10-27 11:54:38 -0500

    Added the skeleton of the 'Resume' page. All that's left is the buttons to actually download it, but I need to figure out how to host the files first.

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,16 @@
+export default function Resume() {
+    return <>
+        <header>
+            <h1>
+                Resume
+            </h1>
+            <p>
+                Choose your format.
+            </p>
+        </header>
+        <main>
+            {/* TODO: Add tile button to download PDF. */}
+            {/* TODO: Add tile button to download Word doc. */}
+        </main>
+    </>
+}
