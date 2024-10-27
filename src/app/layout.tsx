7f74e8f27e0ae38794a453422268267c2689a6ca
Collin Vesel <cvesel217@gmail.com>
2024-10-27 11:18:58 -0500

    Removed unused tiles from the homescreen and rearanged/resized the remaining ones so the page still looks good. The old code is commented-out, not removed, and this is because I plan on adding the tiles back in in the future.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,12 +37,12 @@ export default function RootLayout({
           <Tile title="Bio" linkTo="/bio" tileStyle={styles.BioTile}/>
           <Tile title="Resume" linkTo="/resume" tileStyle={styles.ResumeTile}/>
           <Tile title="Socials" linkTo="/socials" tileStyle={styles.SocialsTile}/>
-          <Tile title="Coming Soon" tileStyle={`${styles.FeaturedTile} ${styles.DisabledTile}`}/>
-          <Tile title="Coming Soon" tileStyle={`${styles.ProjectsTile} ${styles.DisabledTile}`}/>
-          <Tile title="Coming Soon" tileStyle={`${styles.ToyboxTile} ${styles.DisabledTile}`}/>
-          <Tile title="Coming Soon" tileStyle={`${styles.BlogTile} ${styles.DisabledTile}`}/>
-          <Tile title="Coming Soon" tileStyle={`${styles.ThoughtsTile} ${styles.DisabledTile}`}/>
-          <Tile title="Coming Soon" tileStyle={`${styles.NotMineTile} ${styles.DisabledTile}`}/>
+          {/* <Tile title="Featured Updates" tileStyle={`${styles.FeaturedTile} ${styles.DisabledTile}`}/>
+          <Tile title="All Projects" tileStyle={`${styles.ProjectsTile} ${styles.DisabledTile}`}/>
+          <Tile title="Toybox" tileStyle={`${styles.ToyboxTile} ${styles.DisabledTile}`}/>
+          <Tile title="Blog Posts" tileStyle={`${styles.BlogTile} ${styles.DisabledTile}`}/>
+          <Tile title="Thoughts" tileStyle={`${styles.ThoughtsTile} ${styles.DisabledTile}`}/>
+          <Tile title="Not Mine, But Cool" tileStyle={`${styles.NotMineTile} ${styles.DisabledTile}`}/> */}
         </main>
         {/* <footer className={styles.CreditNotice}>
           <small>Copyright notice for bg img goes here.</small>
