112f2dd02bdedc8629a5c1c453a91d2cb35c2be2
Collin Vesel <cvesel217@gmail.com>
2025-05-15 21:34:06 -0500

    Updated the deployment script because on the server's end, I replaced PM2 with a startup daemon to reduce the number of dependencies.

--- deploy-remote.sh
+++ deploy-remote.sh
@@ -3,19 +3,9 @@
 # This script runs on the remote server to unpack and run the built application.
 # It may be merged into the "deploy.sh" script to reduce complecxity.
 
-# Setup
-export PATH=$PATH:/home/admin/.local/share/fnm
-eval "`fnm env`"
-
 # Extract the files
 mkdir -p ./personal-website-build
 tar -xzvf ./personal-website-build.tar.gz -C ./personal-website-build
 
-# Move into the resulting folder
-cd ./personal-website-build
-
-# Install all packages
-npm install --ignore-scripts --omit=dev
-
-# Start the program with PM2, or restart if an older version is already running.
-[ -z "$(pm2 id "npm run start" | grep -e "[[:digit:]]")" ] && pm2 start "npm run start" --watch
+# Restart the website's daemon.
+sudo systemctl restart admin-collinvesel_me.service
