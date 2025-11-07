b9d12d814e29c7cbdc77457b058232ef54f25acf
Collin Vesel <cvesel217@gmail.com>
2025-11-06 21:33:35 -0600

    Added the first TDD test. It doesn't pass yet, will fix next.

---
+++ server_src/cvesel_srv_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestServePages(t *testing.T) {
+	// When I request a page, it should return that page.
+	t.Run("returns the requested page", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/testpage", nil)
+		response := httptest.NewRecorder()
+
+		// Assume this is the serevr.
+		CveselServer(response, request)
+
+		got := response.Body.String()
+		want := "test page plz ignore"
+
+		if got != want {
+			t.Errorf("wrong page contents, got %q, want %q", got, want)
+		}
+	})
+}
---
+++ server_src/go.mod
@@ -0,0 +1,3 @@
+module collinvesel.me/cvesel_srv
+
+go 1.25.3
