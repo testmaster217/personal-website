b18f673e3b9aa0f5fbe51891004381dffe8ad2f6
Collin Vesel <cvesel217@gmail.com>
2025-11-09 13:46:03 -0600

    Added more to the description of how things should be served.

--- server_src/cvesel_srv.go
+++ server_src/cvesel_srv.go
@@ -17,11 +17,15 @@ func (srv *CveselServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func (srv *CveselServer) getPage(path string) string {
 	// Should do different things depending on what is requested:
-	// - If the path ends with a "." followed by anything other than "html" or "htm", it should serve the file as is.
-	// - If the path ends with ".html", ".htm", or "/", redirect to the equivalent path without the trailing thing.
+	// - If the path ends with a "." followed by anything other than "html" or "htm", 
+	// - - If the file is executable, it should be executed, and the results should be served.
+	// - - Otherwise, the file should be served as is.
+	// - If the requested path is empty or "/index[.htm|.html]" (possibly with a "/" at the end), redirect to an empty path if necessary, and serve the contents of "index.html".
+	// - If this does not apply, and the path ends with ".html", ".htm", or "/", redirect to the equivalent path without the trailing thing.
 	// - If the path ends with anything else, serve the file that has that name with ".html" added onto the end.
-	// TODO: Add special treatment for the homepage/"index.html".
-	// TODO: Add treatment for HTMX vs non-HTMX requests.
+	// Whatever gets served after the above are evaluated should be served differently depending on whether the request came from HTMX or not, as indicated by the "HX-Request" header:
+	// - If the header is msising or set to "false", combine the data to be served with a base page and serve that.
+	// - Otherwise, serve the data as is.
 	res, _ := os.ReadFile(fmt.Sprintf("%s%s.html", srv.docRoot, path))
 	return string(res)
 }
