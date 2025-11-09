050af36561131745197b6931a6929c0e1fe89c67
Collin Vesel <cvesel217@gmail.com>
2025-11-09 12:04:50 -0600

    Added some comments explaining how the function for getting a page to serve is supposed to work. Will worry about actually making it do that later.

--- server_src/cvesel_srv.go
+++ server_src/cvesel_srv.go
@@ -16,6 +16,12 @@ func (srv *CveselServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func (srv *CveselServer) getPage(path string) string {
+	// Should do different things depending on what is requested:
+	// - If the path ends with a "." followed by anything other than "html" or "htm", it should serve the file as is.
+	// - If the path ends with ".html", ".htm", or "/", redirect to the equivalent path without the trailing thing.
+	// - If the path ends with anything else, serve the file that has that name with ".html" added onto the end.
+	// TODO: Add special treatment for the homepage/"index.html".
+	// TODO: Add treatment for HTMX vs non-HTMX requests.
 	res, _ := os.ReadFile(fmt.Sprintf("%s%s.html", srv.docRoot, path))
 	return string(res)
 }
