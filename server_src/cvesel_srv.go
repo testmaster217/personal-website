e72286b6904d90b963cb1c994cab1b3889e352ea
Collin Vesel <cvesel217@gmail.com>
2025-11-10 08:23:23 -0600

    Marked a feature in the big comment about how the server should behave as cut because I won't actually need it.

--- server_src/cvesel_srv.go
+++ server_src/cvesel_srv.go
@@ -22,7 +22,7 @@ func (srv *CveselServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func (srv *CveselServer) getPage(path string) string {
 	// Should do different things depending on what is requested:
 	// - If the path ends with a "." followed by anything other than "html" or "htm", 
-	// - - If the file is executable and is not a JS file, it should be executed, and the results should be served.
+	// - - THIS IS CUT: If the file is executable and is not a JS file, it should be executed, and the results should be served.
 	// - - Otherwise, the file should be served as is.
 	// - If the requested path is empty or "/index[.htm|.html]" (possibly with a "/" at the end), redirect to an empty path if necessary, and serve the contents of "index.html".
 	// - If this does not apply, and the path ends with ".html", ".htm", or "/", redirect to the equivalent path without the trailing thing.
