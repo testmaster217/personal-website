6edea4d5ed8b33bdcadd22131c390b30dbec39b1
Collin Vesel <cvesel217@gmail.com>
2025-11-10 20:18:26 -0600

    Marked everything in the big comment that I haven't done yet with a 'TODO'.

--- server_src/cvesel_srv.go
+++ server_src/cvesel_srv.go
@@ -24,12 +24,12 @@ func (srv *CveselServer) getPage(path string) string {
 	// - If the path ends with a "." followed by anything other than "html" or "htm", 
 	// - - THIS IS CUT: If the file is executable and is not a JS file, it should be executed, and the results should be served.
 	// - - Otherwise, the file should be served as is.
-	// - If the requested path is empty or "/index[.htm|.html]" (possibly with a "/" at the end), redirect to an empty path if necessary, and serve the contents of "index.html".
-	// - If this does not apply, and the path ends with ".html", ".htm", or "/", redirect to the equivalent path without the trailing thing.
-	// - If the path ends with anything else, serve the file that has that name with ".html" added onto the end.
+	// - TODO: If the requested path is empty, "/", or "/index[.htm|.html]" (possibly with a "/" at the end), redirect to an empty path if necessary, and serve the contents of "index.html".
+	// - TODO: If this does not apply, and the path ends with ".html", ".htm", or "/", redirect to the equivalent path without the trailing thing.
+	// - If the path does not end with a file extension or a "/", serve the file that has that name with ".html" added onto the end.
 	// Whatever gets served after the above are evaluated should be served differently depending on whether the request came from HTMX or not, as indicated by the "HX-Request" header:
-	// - If the header is msising or set to "false", combine the data to be served with a base page and serve that.
-	// - Otherwise, serve the data as is.
+	// - PARTIAL TODO; DOESN'T WORK WITH PARTIAL PAGES YET: If the header is msising or set to "false", combine the data to be served with a base page and serve that.
+	// - TODO: Otherwise, serve the data as is.
 	if isFileRegexp.MatchString(path) {
 		res, _ := os.ReadFile(fmt.Sprintf("%s%s", srv.docRoot, path))
 		return string(res)
